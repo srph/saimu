ff2f60298bf7f57dd53fa9dbc23c498a65500115
srph <seraphipod@gmail.com>
2017-03-12 08:10:31 +0800

    Implement content static

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -9,43 +9,91 @@ export default class HomeView extends React.Component {
 
         <div className="main-layout">
           <aside className="menu">
-            <div className="main-pane">
-              <form>
-                <div className="pane-search">
-                  <input type="text" className="input" placeholder="Search by name" />
-                  <button className="submit">
-                    <i className="fa fa-search" />
-                  </button>
-                </div>
-              </form>
+            <form>
+              <div className="pane-search">
+                <input type="text" className="input" placeholder="Search by name" />
+                <button className="submit">
+                  <i className="fa fa-search" />
+                </button>
+              </div>
+            </form>
 
+            <div className="main-pane">
               <a href="#" className="pane-item -active">
                 <div className="info">
-                  <h1 className="title">JC Ricaro</h1>
-                  <h4 className="subtitle">15,629.00 to go</h4>
+                  <h4 className="title">JC Ricaro</h4>
+                  <h6 className="subtitle">15,629.00 to go</h6>
                 </div>
               </a>
 
               <a href="#" className="pane-item">
                 <div className="info">
-                  <h1 className="title">Brian Bodollo</h1>
-                  <h4 className="subtitle">15,629.00 to go</h4>
+                  <h4 className="title">Brian Bodollo</h4>
+                  <h6 className="subtitle">15,629.00 to go</h6>
                 </div>
               </a>
 
               <a href="#" className="pane-item">
                 <div className="info">
-                  <h1 className="title">Brian Bodollo</h1>
-                  <h4 className="subtitle">
+                  <h4 className="title">John Doe</h4>
+                  <h6 className="subtitle">
                     <i className="fa fa-check" />
                     Paid Up!
-                  </h4>
+                  </h6>
                 </div>
               </a>
             </div>
           </aside>
 
           <div className="content">
+            <div className="main-content-menu">
+              <div className="heading">
+                <h2 className="title">John Doe</h2>
+                <h4 className="sub">2,739.00</h4>
+              </div>
+
+              <a href="#" className="button -primary -rounded">
+                New Record
+              </a>
+            </div>
+
+            <div className="main-content-section">
+              <h4 className="heading">
+                2017
+              </h4>
+
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Status</th>
+                    <th>Amount</th>
+                    <th>Date Added</th>
+                    <th>Note</th>
+                    <th>&nbsp;</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td>
+                      <span className="tag-type -danger">
+                        Unpaid
+                      </span>
+                    </td>
+                    <td>
+                      2,739.00
+                    </td>
+                    <td>
+                      July 30, 2013
+                    </td>
+                    <td>
+                      November Salary
+                    </td>
+                    <td>&nbsp;</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
