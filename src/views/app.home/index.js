9cd2f7320d14d1c3527000e1c1050e54b1bd4fcc
srph <seraphipod@gmail.com>
2017-04-02 16:55:49 +0800

    Implement empty debtor state

--- src/views/app.home/index.js
+++ src/views/app.home/index.js
@@ -1,26 +1,58 @@
-import React, {PropTypes} from 'react';
-import {ipcRenderer as ipc} from 'electron';
+import React from 'react';
 import Helmet from 'react-helmet';
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
 
-export default class HomeView extends React.Component {
+class HomeView extends React.Component {
   render() {
     return (
       <div>
         <Helmet title="Select a debtor" />
 
-        <div className="empty-state">
-          <img src="img/nerd.svg" className="symbol" />
-
-          <h5 className="main">
-            Debtor who?!
-          </h5>
-
-          <h2 className="sub">
-            You haven't selected a debtor yet!
-          </h2>
-        </div>
+        {this.props.debtors.length ? <Unselected /> : <Empty />}
       </div>
     );
   }
 }
+
+function Unselected() {
+  return (
+    <div className="empty-state">
+      <img src="img/nerd.svg" className="symbol" />
+
+      <h5 className="main">
+        Debtor who?!
+      </h5>
+
+      <h2 className="sub">
+        You haven't selected a debtor yet!
+      </h2>
+    </div>
+  )
+}
+
+function Empty() {
+  return (
+    <div className="empty-state">
+      <img src="img/formal.svg" className="symbol" />
+
+      <h5 className="main">
+        No debtors
+      </h5>
+
+      <h2 className="sub">
+        You haven't registered a debtor yet!
+      </h2>
+
+      <div className="action">
+        <Link to="/d/create" className="button -primary -rounded">
+          New debtor
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default connect(state => ({
+  debtors: state.debtors.data
+}))(HomeView)
